8b11cb8ac2270d0d2440b988d50c1ecd2c108ec7
Chae-vin <arvin.santillan@cit.edu>
2023-11-16 17:11:00 +0800

    my code

--- src/index.js
+++ src/index.js
@@ -10,12 +10,13 @@ import Header from "./Header";
 import ProfileEdit from "./ProfileEdit";
 import ProfileView from "./ProfileView";
 import AdminProfileView from "./AdminProfileView";
+import Signup from "./Signup";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Start />
+    <ProfileView />
   </React.StrictMode>
 );
 
